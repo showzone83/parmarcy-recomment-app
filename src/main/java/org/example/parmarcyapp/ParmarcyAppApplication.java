7460e38744b1a70822c7da1f439c9d990690f77f
ByungJin Park <bjpark@pnjco.co.kr>
2024-03-02 16:59:45 +0900

    first commit

---
+++ src/main/java/org/example/parmarcyapp/ParmarcyAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.parmarcyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParmarcyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParmarcyAppApplication.class, args);
+    }
+
+}
